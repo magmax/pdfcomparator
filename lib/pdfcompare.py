b0971027232cf8d5d1588e47f9f88fa02f6ae82c
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2011-10-16 19:46:21 +0200

    green. Identifica que dos documentos son iguales por su número de páginas.

--- lib/pdfcompare.py
+++ lib/pdfcompare.py
@@ -1,18 +1,40 @@
 #!/usr/bin/python
 # -*- coding:utf-8; tab-width:4; mode:python -*-
 
+import os
 import sys
 import poppler
 
 
 class ComparePDF(object):
-    def compare(self):
-        return sys.argv[1] == sys.argv[2]
+    def compare(self, a, b):
+        if a == b:
+            return True
+
+        doc_a = self.__load_file(a)
+        doc_b = self.__load_file(b)
+
+        return doc_a.get_n_pages() == doc_b.get_n_pages()
+
+    def __load_file(self, filename):
+        uri = "file://{0}".format(os.path.realpath(filename))
+        return poppler.document_new_from_file(uri, None)
 
 
-if __name__ == '__main__':
+
+def print_help():
+    print "Format: "
+    exit(1)
+
+def main():
     c = ComparePDF()
-    if c.compare():
+    if len (sys.argv) < 3:
+        print_help()
+
+    if c.compare(sys.argv[1], sys.argv[2]):
         exit(0)
     else:
         exit(2)
+
+if __name__ == '__main__':
+        main()
