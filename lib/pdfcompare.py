5c41dc95db4c329f802cc1f7be6822a2841b6904
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2011-10-16 17:30:17 +0200

    green. 2 tests pasados

--- lib/pdfcompare.py
+++ lib/pdfcompare.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python
 # -*- coding:utf-8; tab-width:4; mode:python -*-
 
+import sys
+import poppler
+
+
+class ComparePDF(object):
+    def compare(self):
+        return sys.argv[1] == sys.argv[2]
+
+
 if __name__ == '__main__':
-    exit(0)
+    c = ComparePDF()
+    if c.compare():
+        exit(0)
+    else:
+        exit(2)
