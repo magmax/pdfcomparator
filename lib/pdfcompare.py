d897700f290bd1b3ca4bc71bffa3f5dc8e82ac69
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2011-10-18 08:10:21 +0200

    green. Es funcional.

--- lib/pdfcompare.py
+++ lib/pdfcompare.py
@@ -4,34 +4,76 @@
 import os
 import sys
 import poppler
-
+import cairo
+import io
 
 class ComparePDF(object):
-    def compare(self, a, b):
-        if a == b:
+    def __init__(self, file_a, file_b):
+        self.file_a = file_a
+        self.file_b = file_b
+
+    def compare(self):
+        try:
+            return self.__try_compare()
+        except:
+            return False
+
+    def __try_compare(self):
+        doc_a = self.__load_file(self.file_a)
+        doc_b = self.__load_file(self.file_b)
+
+        if not self.__have_same_page_number(doc_a, doc_b):
+            return False
+
+        if not self.__all_pages_are_equal(doc_a, doc_b):
+            return False
+
+        return True
+
+    def __have_same_page_number(self, doc_a, doc_b):
+        pages_a = doc_a.get_n_pages()
+        pages_b = doc_b.get_n_pages()
+
+        if pages_a is pages_b:
             return True
 
-        doc_a = self.__load_file(a)
-        doc_b = self.__load_file(b)
+        print 'Different number of pages ({0} vs {1})'.format(pages_a, pages_b)
 
-        return doc_a.get_n_pages() == doc_b.get_n_pages()
+        return False
+
+    def __all_pages_are_equal(self, doc_a, doc_b):
+        for i in xrange(doc_a.get_n_pages()):
+            pagea0 = doc_a.get_page(i)
+            pageb0 = doc_b.get_page(i)
+
+            if self.__render(pagea0) != self.__render(pageb0):
+                print 'Page {0} is different.'.format(i+1)
+                return False
+        return True
 
     def __load_file(self, filename):
-        uri = "file://{0}".format(os.path.realpath(filename))
+        path = os.path.realpath(filename)
+        uri = "file://{0}".format(path)
         return poppler.document_new_from_file(uri, None)
 
+    def __render(self, page):
+        size = page.get_size()
+        surface = cairo.ImageSurface (cairo.FORMAT_ARGB32, int(size[0]), int(size[1]))
+        context = cairo.Context(surface)
+        page.render(context)
+        return surface.get_data()
 
 
 def print_help():
-    print "Format: "
+    print "Format: pdfcompare.py file1 file2"
     exit(1)
 
 def main():
-    c = ComparePDF()
     if len (sys.argv) < 3:
         print_help()
 
-    if c.compare(sys.argv[1], sys.argv[2]):
+    c = ComparePDF(sys.argv[1], sys.argv[2])
+    if c.compare():
         exit(0)
     else:
         exit(2)
